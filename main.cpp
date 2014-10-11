e2b80585f76b61358d4d451421a27f099efb794b
Daedalus93 <fredx96@gmail.com>
2014-10-11 18:44:04 +0100

    Fixed relevant issues with the backtracking step. Can now read from files

--- main.cpp
+++ main.cpp
@@ -3,28 +3,74 @@
 #include <fstream>
 #include <math.h>
 #include <string>
+#include <fstream>
+#include <sstream> 
 
+void fillMatrixFromFile(std::string path, std::vector< std::vector<int> > &matrix, std::string &x, std::string &y);
+void createMatrix(int l, int c, std::vector< std::vector<int> > &matrix);
 void processMatrix(std::vector< std::vector<int> > &matrix, std::string x, std::string y);
 std::string backtrack(std::vector< std::vector<int> > &matrix, std::string x, std::string y, int i, int j);
-void createMatrixFromFile(std::string path, std::vector< std::vector<int> > &matrix, std::string x, std::string y);
-void createMatrix(int l, int c, std::vector< std::vector<int> > &matrix);
 void printMatrix(std::vector< std::vector<int> > &matrix);
 short cost(int x);
 
 
 int main() {
-	std::vector< std::vector<int> > matrix;
-	std::string y = "BDCABA";
-	std::string x = "ABCBDAB";
+    std::vector< std::vector<int> > matrix;
+    std::string x = "ABCBDAB";
+    std::string y = "BDCABA";    
 
-	createMatrixFromFile("", matrix, x, y);
-	processMatrix(matrix, x, y);
-	printMatrix(matrix);
-	std::string result = backtrack(matrix, x, y, x.size(), y.size()); 
-
-	std::cout << result << std::endl;
-	return 0;
+	fillMatrixFromFile("public-instances/ex10.15.in", matrix, x, y);
 	
+
+    // Example:
+    //createMatrix(y.size()+1, x.size()+1, matrix);
+    //processMatrix(matrix, x, y);
+
+    //Warning: printing to stdout may be slow
+	//printMatrix(matrix);
+
+	std::string result = backtrack(matrix, x, y, x.size(), y.size()); 
+	std::cout << result << std::endl;
+
+	return 0;	
+}
+
+void fillMatrixFromFile(std::string path, std::vector< std::vector<int> > &matrix, std::string &x, std::string &y) {
+    std::ios_base::sync_with_stdio (false);
+
+    std::stringstream ss;
+    std::ifstream file (path.c_str(), std::ifstream::in);
+
+	if (file.is_open()) {
+		std::string line;
+		std::getline(file, line);
+
+        ss << line;
+        
+        int nLines, nCols;
+
+        ss >> nLines >> nCols;
+
+        std::cout << "Number of lines: " << nLines << std::endl;
+        std::cout << "Number of cols: " << nCols << std::endl;
+
+        
+        std::getline(file, x);
+        std::getline(file, y);
+
+        std::cout << "X: " << x << std::endl;
+        std::cout << "Y: " << y << std::endl;
+
+        createMatrix(y.size()+1, x.size()+1, matrix);
+        processMatrix(matrix, x, y);
+	}
+}
+
+void createMatrix(int l, int c, std::vector< std::vector<int> > &matrix) {
+	for (int i = 0; i < c; ++i) {
+		std::vector<int> column(l, 0);
+		matrix.push_back(column);
+	}
 }
 
 void processMatrix(std::vector< std::vector<int> > &matrix, std::string x, std::string y) {
@@ -52,37 +98,17 @@ std::string backtrack(std::vector< std::vector<int> > &matrix, std::string x, st
 
 	if(i==0 || j==0) {
 		return "" ;
-	} else {
-		if(x[i-1] == y[j-1]) {
-			return backtrack(matrix, x, y, i-1, j-1).append(std::string(1, x[i-1]));
+    } else if(x[i-1] == y[j-1]) {
+        return backtrack(matrix, x, y, i-1, j-1).append(std::string(1, x[i-1]));
+    } else {
+		if(matrix[i][j-1] == matrix[i-1][j] || matrix[i][j-1] > matrix[i-1][j]) {
+			return backtrack(matrix, x, y, i, j-1);
 		} else {
-			return backtrack(matrix, x, y, i, j-1);	
+			return backtrack(matrix, x, y, i-1, j);	
 		}	
 	}
 }
 
-
-void createMatrixFromFile(std::string path, std::vector< std::vector<int> > &matrix, std::string x, std::string y) {
-	
-	/*int nLines = 0;
-	int nCols = 0;
-
-	std::ifstream file (path);
-	if (file.is_open()) {
-		std::string line;
-		getLine(file, line);
-		//nLines = atoi(line.c_str());
-	}
-	*/
-	createMatrix(y.size()+1, x.size()+1, matrix);
-}
-
-void createMatrix(int l, int c, std::vector< std::vector<int> > &matrix) {
-	for (int i = 0; i < c; ++i) {
-		std::vector<int> column(l, 0);
-		matrix.push_back(column);
-	}
-}
 
 void printMatrix(std::vector< std::vector<int> > &matrix) {
 	for (int i = 0; i < matrix.size(); ++i) {
